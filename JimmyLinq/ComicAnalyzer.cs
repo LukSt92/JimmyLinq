c83fbf8792e75e242ad5b813a9973d1ea109a564
LukSt92 <lukasz.m.stasiak@gmail.com>
2024-08-29 15:59:33 +0200

    Refact: GroupComicsByPrice method in ComicAnalyzer class

--- JimmyLinq/ComicAnalyzer.cs
+++ JimmyLinq/ComicAnalyzer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,11 +19,12 @@ namespace JimmyLinq
         public static IEnumerable<IGrouping<PriceRange, Comic>> GroupComicsByPrice(
                               IEnumerable<Comic> comics, IReadOnlyDictionary<int, decimal> prices)
         {
-            IEnumerable<IGrouping<PriceRange, Comic>> grouped =
-                from comic in comics
-                orderby prices[comic.Issue]
-                group comic by CalculatePriceRange(comic, prices) into priceGroup
-                select priceGroup;
+            var grouped = comics.OrderBy(comic => prices[comic.Issue]).GroupBy(comic => CalculatePriceRange(comic, prices));
+            //IEnumerable<IGrouping<PriceRange, Comic>> grouped =
+            //    from comic in comics
+            //    orderby prices[comic.Issue]
+            //    group comic by CalculatePriceRange(comic, prices) into priceGroup
+            //    select priceGroup;
             return grouped;
         }
         public static IEnumerable<string> GetReviews(IEnumerable<Comic> comics, IEnumerable<Review> reviews)
